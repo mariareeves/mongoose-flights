730d6da41f5e5e4679183eeac1a5b50a1e16aaad
mariareeves <mariapaula.zimmers@gmail.com>
2023-01-14 20:59:45 -0700

    add create functionality

--- controller/flights.js
+++ controller/flights.js
@@ -1,9 +1,26 @@
-const Flights = require('../models/flight')
+const Flight = require('../models/flight');
 
 module.exports = {
+    index,
     new: newFlight,
+    create,
 };
 
+function index(req, res) {
+    Flight.find({}, function (err, flights) {
+        console.log(flights)
+        res.render('flights/index', { flights })
+    });
+}
 function newFlight(req, res) {
     res.render('flights/new');
+}
+
+function create(req, res) {
+    const flight = new Flight(req.body)
+    flight.save(function (err) {
+        if (err) return res.redirect('/flights/new')
+        console.log(flight)
+        res.redirect('/flights')
+    })
 }
--- routes/flights.js
+++ routes/flights.js
@@ -7,7 +7,11 @@ var router = express.Router();
 const flightsCtrl = require('../controller/flights');
 
 // ALL actual paths begin with '/flights
+// GET /flights
+router.get('/', flightsCtrl.index);
 // GET /flights/new
 router.get('/new', flightsCtrl.new);
+// POST /flights
+router.post('/', flightsCtrl.create);
 
 module.exports = router;
